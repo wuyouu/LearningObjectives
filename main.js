0489dfc3ac6e45b1e1511ece659a046c29ab9a34
WilliamWu <995347291@qq.com>
2021-12-24 16:17:19 +0800

    添加对多个sheet的处理

--- main.js
+++ main.js
@@ -1,37 +1,40 @@
 const xlsx = require('node-xlsx')
 const moment = require('moment')
 const fs = require('fs')
+const { title } = require('process')
 
 // 读取文件
 const sheets = xlsx.parse('./temp.xlsx', { cellDates: true })
 // 获取开班日期
 const START_DATE = process.env.START_DATE
 
-const Part1 = [['日期', '学习目标', '对应视频']]
-
+const titles = ['日期', '学习目标', '对应视频']
+const sheetsLists = []
+let j = 0
 sheets.forEach(sheet => {
+  const sheetData = [titles.slice()]
   const data = sheet.data
   const len = data.length
-  for(let i = 1; i < len; i++) {
+  let i
+  for(i = 1; i < len; i++,j++) {
     const row = data[i]
     if(row && row.length){
-      Part1.push([
-        moment(START_DATE).add(i, 'day').format('YYYY/MM/DD'), // 日期
+      sheetData.push([
+        moment(START_DATE).add(j, 'day').format('YYYY/MM/DD'), // 日期
         formatNR(row[1]), // 学习目标
         formatNR(row[2]) // 对应视频
       ])
     }
   }
+  sheetsLists.push({
+    name: sheet.name,
+    data: sheetData
+  })
 })
 
 // 写入文件
-let buffer = xlsx.build([{
-  name: 'Part1',
-  data: Part1
-}], {
-  sheetOptions: {
-    '!cols': [{ wch: 20 }, { wch: 45 }, { wch: 35 }]
-  }
+let buffer = xlsx.build(sheetsLists, {
+  sheetOptions: { '!cols': [{ wch: 20 }, { wch: 45 }, { wch: 35 }] }
 })
 fs.writeFileSync('./新生成的学习计划.xlsx', buffer, { flag: 'w'})
 
